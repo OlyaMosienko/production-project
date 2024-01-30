fd3965d2028a7d5b8dfb319d44f0d11721035dab
OlyaMosienko <olya.ololoeva81@mail.ru>
2024-01-30 12:18:05 +0300

    add babel, add extract plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import { render } from "react-dom";
 import classes from './index.module.scss'
-import App from "./App";
+import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
-import ThemeProvider from "./styles/theme/ThemeProvider";
+import { ThemeProvider } from "app/providers/ThemeProvider";
+
+import "./shared/config/i18n/i18n";
 
 render(
     <BrowserRouter>
